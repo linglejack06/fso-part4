b074d817ff28b1e444a5dd59c4762a95104c1d57
Jack Lingle <captainjack0818@gmail.com>
2023-06-21 17:26:56 -0400

    add middleware

--- blog/app.js
+++ blog/app.js
@@ -1,16 +1,23 @@
+/* eslint-disable import/no-unresolved */
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('./utils/config');
+const { MONGODB_URI } = require('./utils/config');
 const logger = require('./utils/logger');
+const blogRouter = require('./controllers/blogs');
 
 const app = express();
 
 mongoose.set('strictQuery', false);
-mongoose.connect(config.MONGODB_URI)
+mongoose.connect(MONGODB_URI)
   .then(() => {
     logger.info('Connected to database');
   })
   .catch((error) => {
     logger.error('error connecting to database: ', error.message);
   });
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogRouter);
+module.exports = app;
