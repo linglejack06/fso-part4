26d83f6275c23b18ff5fe60d9710e67b565940d1
Jack Lingle <captainjack0818@gmail.com>
2023-06-21 18:04:42 -0400

    add total likes function

--- blog/tests/listHelper.test.js
+++ blog/tests/listHelper.test.js
@@ -5,3 +5,25 @@ test('dummy returns length of blogs', () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(5);
 });
+describe('total likes', () => {
+  const blogs = [
+    { likes: 5 },
+    { likes: 2 },
+    { likes: 4 },
+  ];
+
+  test('adds up all likes', () => {
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(11);
+  });
+  test('handles length of 1', () => {
+    blogs.pop();
+    blogs.pop();
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(5);
+  });
+  test('handles empty array', () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
+});
--- blog/utils/listHelper.js
+++ blog/utils/listHelper.js
@@ -1,5 +1,7 @@
 const dummy = (blogs) => blogs.length;
-const totalLikes = (blogs) => blogs.reduce((sum, acc) => sum + acc.likes, 0);
+const totalLikes = (blogs) => (blogs.length === 0
+  ? 0
+  : blogs.reduce((sum, acc) => sum + acc.likes, 0));
 module.exports = {
-  dummy, totalLikes
-}
+  dummy, totalLikes,
+};
