60a49ba76a83e15e978a1ef0fb728256b50ab234
Jack Lingle <captainjack0818@gmail.com>
2023-06-21 17:36:10 -0400

    update blogSchema to use Number type for likes

--- blog/models/blog.js
+++ blog/models/blog.js
@@ -4,7 +4,7 @@ const blogSchema = mongoose.Schema({
   author: String,
   title: String,
   url: String,
-  likes: String,
+  likes: Number,
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
