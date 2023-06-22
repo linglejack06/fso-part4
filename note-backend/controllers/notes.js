4c152255a0a46d71ef3367e3c1ec724b68a13d06
Jack Lingle <captainjack0818@gmail.com>
2023-06-22 17:23:03 -0400

    refactor delete and get one note routes

--- note-backend/controllers/notes.js
+++ note-backend/controllers/notes.js
@@ -1,25 +1,31 @@
 const notesRouter = require('express').Router();
+const note = require('../models/note');
 const Note = require('../models/note');
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({});
   return res.json(notes);
 });
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        res.json(note);
-      } else {
-        res.status(400).end();
-      }
-    })
-    .catch((error) => next(error));
+notesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(404).end();
+    }
+  }
+  catch (error) {
+    next(error);
+  }
 });
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(() => res.status(204).end())
-    .catch((error) => next(error));
+notesRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await note.findByIdAndRemove(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 });
 notesRouter.post('/', async (req, res, next) => {
   const { body } = req;
