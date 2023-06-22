354788e64a56e2363775b809ed59cb6b4611ea8a
Jack Lingle <captainjack0818@gmail.com>
2023-06-22 17:26:29 -0400

    refactor routes without try catch

--- note-backend/controllers/notes.js
+++ note-backend/controllers/notes.js
@@ -6,42 +6,29 @@ notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({});
   return res.json(notes);
 });
-notesRouter.get('/:id', async (req, res, next) => {
-  try {
-    const note = await Note.findById(req.params.id);
-    if (note) {
-      res.json(note);
-    } else {
-      res.status(404).end();
-    }
-  }
-  catch (error) {
-    next(error);
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
   }
 });
-notesRouter.delete('/:id', async (req, res, next) => {
-  try {
-    await note.findByIdAndRemove(req.params.id);
-    res.status(204).end();
-  } catch (error) {
-    next(error);
-  }
+notesRouter.delete('/:id', async (req, res) => {
+  await note.findByIdAndRemove(req.params.id);
+  res.status(204).end();
 });
-notesRouter.post('/', async (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const { body } = req;
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
-  try {
-    const savedNote = await note.save();
-    res.status(201).json(savedNote);
-  } catch(error) {
-    next(error);
-  }
+  const savedNote = await note.save();
+  res.status(201).json(savedNote);
 });
-notesRouter.put('/:id', (req, res, next) => {
+notesRouter.put('/:id', (req, res) => {
   const { content, important } = req.body;
   Note.findByIdAndRemove(
     req.params.id,
@@ -50,8 +37,7 @@ notesRouter.put('/:id', (req, res, next) => {
   )
     .then((updatedNote) => {
       res.json(updatedNote);
-    })
-    .catch((error) => next(error));
+    });
 });
 
 module.exports = notesRouter;
