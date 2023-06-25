2ea5dfb2c36d09e91bb105cbae49bbf160f3ac6f
Jack Lingle <captainjack0818@gmail.com>
2023-06-25 08:56:57 -0400

    add tests to check errors

--- blog/tests/userApi.test.js
+++ blog/tests/userApi.test.js
@@ -1,6 +1,8 @@
 const supertest = require('supertest');
 const app = require('../app');
-const { initialUsers, usersInDb, initializeDb, closeDb } = require('./testHelper');
+const {
+  initialUsers, usersInDb, initializeDb, closeDb, 
+} = require('./testHelper');
 
 const api = supertest(app);
 
@@ -23,6 +25,39 @@ describe('POST routes', () => {
     const users = await usersInDb();
     const usernames = users.map((user) => user.username);
     expect(usernames).toContain('kling');
+  });
+  test('Non-unique username returns with status code 400', async () => {
+    const duplicateUser = {
+      username: 'jling',
+      password: 'fake',
+    };
+    await api
+      .post('/api/users')
+      .send(duplicateUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+    const users = await usersInDb();
+    expect(users).toHaveLength(initialUsers.length);
+  });
+  test('missing username or password returns with status code 400', async () => {
+    const dumbUser = {
+      name: 'jack',
+      username: 'jack',
+    };
+    await api
+      .post('/api/users')
+      .send(dumbUser)
+      .expect(400);
+    const userWithoutUsername = {
+      name: 'jack',
+      password: 'hi',
+    };
+    await api
+      .post('/api/users')
+      .send(userWithoutUsername)
+      .expect(400);
+    const users = await usersInDb();
+    expect(users).toHaveLength(initialUsers.length);
   });
 });
 
