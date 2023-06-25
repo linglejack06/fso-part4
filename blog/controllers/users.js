dc5a08a73fdd3282144bd58924f60bdf35db3f88
Jack Lingle <captainjack0818@gmail.com>
2023-06-25 09:00:31 -0400

    add check for missing pw or username before init

--- blog/controllers/users.js
+++ blog/controllers/users.js
@@ -6,6 +6,9 @@ const User = require('../models/user');
 usersRouter.post('/', async (req, res, next) => {
   const { username, name, password } = req.body;
   const saltRounds = 15;
+  if (!password || !username) {
+    return res.status(400).json({ error: 'missing username or password' });
+  }
   try {
     const passwordHash = await bcrypt.hash(password, saltRounds);
     const user = new User({ username, name, passwordHash });
